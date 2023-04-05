18edbe46cb82d9ca121b1b9d56cc807516a91879
Pisethsek <pisethsek9899@gmail.com>
2023-04-05 09:06:30 +0700

    banking hw

---
+++ src/bankingHomework/BankingService.java
@@ -0,0 +1,28 @@
+package bankingHomework;
+
+public class BankingService {
+
+    SavingAccount savingAccount;
+    CreditCardAccount creditCardAccount;
+
+    BankingService(){
+        savingAccount = new SavingAccount();
+        creditCardAccount = new CreditCardAccount();
+    }
+    BankingService(SavingAccount savingAccount, CreditCardAccount creditCardAccount){
+        this.savingAccount = savingAccount;
+        this.creditCardAccount = creditCardAccount;
+    }
+    double showBalance(){
+        return creditCardAccount.checkBalance();
+    }
+    void addCreditCardMoney(double amount ){
+        savingAccount.deposit(amount);
+    }
+    void  addMoney(double amount){
+        savingAccount.deposit(amount);
+    }
+    void withdrawMoney(double amount){
+        savingAccount.withdraw(amount);
+    }
+}
--- src/makingAnnotation/Application.java
+++ src/makingAnnotation/Application.java
@@ -3,13 +3,11 @@ package makingAnnotation;
 public class Application {
     public static void main(String[] args) {
         AppleProduct obj = new AppleProduct(1001, "13 pro", 2023, new AppleBluetooth());
-
         Class<?> clazz = obj.getClass();
         if (clazz.isAnnotationPresent(SmartPhone.class)){
             System.out.println("It is the group of smartphone.");
         }
-
         System.out.println(obj);
-        
+
     }
 }
